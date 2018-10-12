e37684e5e2e4c8a7ce48768c8b391028a5634606
abstract-animation-9914 <vm930@hotmail.com>
2018-10-12 13:07:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file.
+//use map to perform a set of string transformations --write lowerCaseDrivers
+
+
+
+const lowerCaseDrivers = drivers.map(d => d.toLowerCase())
+
+
+// function lowerCaseDrivers() {
+  
+// }
